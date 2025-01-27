44f4d75cc9c7b00bcf6163a8bc865166e83afb24
Myriam <myriamalbustany@gmail.com>
2025-01-27 19:03:39 +0200

    ui fix: fixed formatting issues that only happen after reloading the page (footer + header pages)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import MagicButton from "@/components/ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
-import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-[28vh]" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src=""
-          alt=""
-          className="w-full h-full opacity-50"
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take {' '}
@@ -35,26 +26,6 @@ const Footer = () => {
           />
         </a>
       </div>
-
-
-      {/* additional links to LinkedIn and GitHub */}
-      {/*
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">@myrmlbst 2025</p>
-      </div>
-
-      <div className="flex items-center md:gap-3 gap-6">
-        {socialMedia.map((info) => (
-          <div
-            key={info.id}
-            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-          >
-            <img src={info.img} width={20} height={20} />
-          </div>
-        ))}
-      </div>
-      */}
-
 
     </footer>
   )
